ca55c96b1e2ccaadedc57eb546ab530fc25fbf37
Calebs <calebsconsults@gmail.com>
2022-05-08 09:11:35 +0100

    UI Layout fixes

--- js/main.js
+++ js/main.js
@@ -1,46 +1,22 @@
-// jQuery(document).ready(function($) {
-//     $('[data-toggle="tooltip"]').tooltip();
+jQuery(document).ready(function($) {
+    $('[data-toggle="tooltip"]').tooltip();
 
-//     // Show and hide password, user login
-//     var visualTrigger = $("#showPass");
-//     var pwd_input = $(".user_pass");
-//     visualTrigger.on("click", function() {
-//         $(this).toggleClass("show_pass");
-//         if (pwd_input.attr("type") == "password") {
-//             pwd_input.attr("type", "text");
-//         } else {
-//             pwd_input.attr("type", "password");
-//         }
-//     });
-// });
+    /* General Navigation*/
+    
+    //open/close primary navigation
+    $('.primary-nav-trigger').on('click', function(event){
+        event.stopPropagation();
+        $('.menu-icon').toggleClass('is-clicked');
+        $('header[role="banner"]').toggleClass('menu-is-open');
+        $('body').toggleClass('is-active');
+        $('body').toggleClass('scroll_lock');
+    });
 
-// const sidebar = document.querySelector(".mainSidebar");
-// const mainContent = document.querySelector(".mainContentWrapper");
-// document.querySelector(".toggleSidebar").onclick = function() {
-//     sidebar.classList.toggle("mainSidebar_shrunk");
-//     mainContent.classList.toggle("mainContentWrapper_Enlarged");
-// };
+    var hideOverlayBtn = $('.overlayToggle');
 
-//  TO TOGGLE SIDEBAR
-const hamburger = document.getElementById("hamburger");
-const closeBtn = document.getElementById("close");
-const overlay = document.querySelector(".overlay-menu");
-const sidebarLinks = document.getElementById("sidebar");
-
-hamburger.addEventListener("click", () => {
-    console.log("open");
-    document.querySelector(".nav--toggle-open").style.display = "none";
-    document.querySelector(".nav--toggle-close").style.display = "block";
-    sidebarLinks.style.display = "block";
-    overlay.style.display = "block";
-    overlay.style.opacity = "0.8";
-});
-
-closeBtn.addEventListener("click", () => {
-    console.log("close");
-    document.querySelector(".nav--toggle-close").style.display = "none";
-    document.querySelector(".nav--toggle-open").style.display = "block";
-    sidebarLinks.style.display = "none";
-    overlay.style.display = "none";
-    overlay.style.opacity = "0";
+    hideOverlayBtn.on('click', function(e) {
+        e.preventDefault();
+        $(this).parents(".widgetMediaHolder").removeClass("has_overlay");
+    });
+    
 });
